44743e5a7c09b64da4955e1129747e1e472c2d4f
Victor200524 <victor.donaire@hotmail.com>
2024-09-13 10:06:58 -0300

    Cadastro do produto feito, mas esta com um erro

--- sistema/src/componentes/Telas/Cadastros/CadProduto.jsx
+++ sistema/src/componentes/Telas/Cadastros/CadProduto.jsx
@@ -4,45 +4,87 @@ import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 import Row from 'react-bootstrap/Row';
+import { useState } from 'react';
 
 export default function Produto(props){
+    const [produto,setProduto] = useState({
+      codigo: 0,
+      descricao:"",
+      precoCusto: 0,
+      precoVenda:0,
+      qtdEstoque:0,
+      urlImagem:"",
+      dataValidade:""
+    });
 
+    const [formValidado,setFormValidado] = useState(false);
+    function manipularSubmissao(evento){
+        const form = evento.currentTarget;
 
+        if(form.checkValidity()){
+            //Cadastro do produto
+            props.listaDeProdutos.push(produto);
+
+            //exibir tabela com o produto incluso
+            props.setExibirTabela(true);
+        }
+        else{
+            setFormValidado(true);
+        }
+        evento.preventDefault(); // vou querer o momento padrão da submissao
+        evento.stopPropagation(); // vou querer parar o momento padrão da submissao
+    }
+    function manipularMudanca(evento){
+      const elemento = evento.target.name;
+      const valor = evento.target.value;
+      setProduto({...produto,[elemento]:valor}); // {...produto} -> é um sprad, onde ele é um espalhador, onde ele despeja o conteudo desse objeto para um novo objeto
+    }
 
     return(
       <div>
         <div>
           <Container>
-            <Form>
+            <Form noValidate validated={formValidado} onSubmit={manipularSubmissao}>
             <Row className="mb-3">
-              <Form.Group as={Col} md="2" controlId="validationCustom01">
+              <Form.Group as={Col} md="2" >
                 <Form.Label>Código</Form.Label>
                 <Form.Control
                   required
+                  id = "codigo"
+                  name = "codigo"
                   type="int"
+                  value={produto.codigo}
+                  onChange={manipularMudanca}
                 />
-                <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                <Form.Control.Feedback type='invalid'>Código Invalido</Form.Control.Feedback>
+                <Form.Control.Feedback type='valid'>Looks good!</Form.Control.Feedback>
               </Form.Group>
 
-              <Form.Group as={Col} md="4" controlId="validationCustom02">
+              <Form.Group as={Col} md="4">
                 <Form.Label>Descrição</Form.Label>
                 <Form.Control
                   required
                   type="text"
-                  defaultValue=""
+                  id = "descricao"
+                  name = "descricao"
+                  value={produto.descricao}
+                  onChange={manipularMudanca}
                 />
                 <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
               </Form.Group>
 
-              <Form.Group as={Col} md="2" controlId="validationCustomUsername">
+              <Form.Group as={Col} md="2" >
                 <Form.Label>Preço de Custo</Form.Label>
                 <InputGroup hasValidation>
                   <InputGroup.Text id="inputGroupPrepend">R$</InputGroup.Text>
                   <Form.Control
                     type="double"
-                    placeholder=""
                     aria-describedby="inputGroupPrepend"
                     required
+                    id = "precoCusto"
+                    name = "precoCusto"
+                    value={produto.precoCusto}
+                    onChange={manipularMudanca}
                   />
                   <Form.Control.Feedback type="invalid">
                     Please choose a e-mail.
@@ -50,14 +92,16 @@ export default function Produto(props){
                 </InputGroup>
               </Form.Group>
 
-              <Form.Group as={Col} md="2" controlId="validationCustomUsername">
+              <Form.Group as={Col} md="2">
                 <Form.Label>Preço de de Venda</Form.Label>
                 <InputGroup hasValidation>
                   <InputGroup.Text id="inputGroupPrepend">R$</InputGroup.Text>
                   <Form.Control
                     type="double"
-                    placeholder=""
-                    aria-describedby="inputGroupPrepend"
+                    id = "precoVenda"
+                    name = "precoVenda"
+                    value={produto.precoVenda}
+                    onChange={manipularMudanca}
                     required
                   />
                   <Form.Control.Feedback type="invalid">
@@ -66,15 +110,18 @@ export default function Produto(props){
                 </InputGroup>
               </Form.Group>
 
-              <Form.Group as={Col} md="2" controlId="validationCustomUsername">
+              <Form.Group as={Col} md="2">
                 <Form.Label>Estoque</Form.Label>
                 <InputGroup hasValidation>
                   <InputGroup.Text id="inputGroupPrepend">+</InputGroup.Text>
                   <Form.Control
-                    type="double"
-                    placeholder=""
+                    type="int"
                     aria-describedby="inputGroupPrepend"
                     required
+                    id = "qtdEstoque"
+                    name = "qtdEstoque"
+                    value={produto.qtdEstoque}
+                    onChange={manipularMudanca}
                   />
                   <Form.Control.Feedback type="invalid">
                     Please choose a e-mail.
@@ -84,21 +131,30 @@ export default function Produto(props){
 
             </Row>
             <Row className="mb-3">
-              <Form.Group as={Col} md="9" controlId="validationCustom03">
+              <Form.Group as={Col} md="9">
                 <Form.Label>URL da imagem</Form.Label>
                 <Form.Control
-                  type="text"
+                    type="link"
+                    required
+                    id = "urlImagem"
+                    name = "urlImagem"
+                    value={produto.urlImagem}
+                    onChange={manipularMudanca}
                 />
                 <Form.Control.Feedback type="invalid">
                   Please provide a valid city.
                 </Form.Control.Feedback>
               </Form.Group>
 
-              <Form.Group as={Col} md="3" controlId="validationCustom04">
+              <Form.Group as={Col} md="3">
                 <Form.Label>Valido até</Form.Label>
                 <Form.Control 
                   type="date" 
                   required 
+                  id = "dataValidade"
+                  name = "dataValidade"
+                  value={produto.dataValidade}
+                  onChange={manipularMudanca}
                 />
                 <Form.Control.Feedback type="invalid">
                   Please provide a valid state.
@@ -121,7 +177,7 @@ export default function Produto(props){
                 {//offset é o deslocamento
                   }
                 <Col md={{offset:1}}> 
-                  <Button variant='outline-success' type='submit' onClick={()=>{
+                  <Button variant='outline-success' onClick={()=>{
                     props.setExibirTabela(true);
                   }} >Voltar</Button>
                 </Col>
